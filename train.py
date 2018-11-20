2405ffd64987fd6f775c3121c6cd0d4f0a821d54
Artem Yankov <yankovai3@gmail.com>
2018-11-20 10:46:06 -0700

    modernized tfconfig

--- train.py
+++ train.py
@@ -2,43 +2,122 @@ import tensorflow as tf
 import os
 import json
 import time
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from clusterone import get_data_path, get_logs_path
 from model_def import make_model_def
 import tiny_imagenet_data_utils as data_utils
 
+tf.logging.set_verbosity(tf.logging.INFO)
 flags = tf.app.flags
 
-#
-# Snippet for distributed learning
-#
-try:
-    task_type = os.environ['JOB_NAME']
-    task_index = int(os.environ['TASK_INDEX'])
-    ps_hosts = os.environ['PS_HOSTS'].split(',')
-    worker_hosts = os.environ['WORKER_HOSTS'].split(',')
-    TF_CONFIG = {
-        'task': {'type': task_type, 'index': task_index},
+def parse_args():
+    """Parse arguments"""
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter,
+                            description='''Train a convolution neural network with MNIST dataset.
+                            For distributed mode, the script will use few environment variables as defaults:
+                            JOB_NAME, TASK_INDEX, PS_HOSTS, and WORKER_HOSTS. These environment variables will be
+                            available on distributed Tensorflow jobs on Clusterone platform by default.
+                            If running this locally, you will need to set these environment variables
+                            or pass them in as arguments (i.e. python mnist.py --job_name worker --task_index 0
+                            --worker_hosts "localhost:2222,localhost:2223" --ps_hosts "localhost:2224").
+                            If these are not set, the script will run in non-distributed (single instance) mode.''')
+
+    # Configuration for distributed task
+    parser.add_argument('--job_name', type=str, default=os.environ.get('JOB_NAME', None), choices=['worker', 'ps'],
+                        help='Task type for the node in the distributed cluster. Worker-0 will be set as master.')
+    parser.add_argument('--task_index', type=int, default=os.environ.get('TASK_INDEX', 0),
+                        help='Worker task index, should be >= 0. task_index=0 is the chief worker.')
+    parser.add_argument('--ps_hosts', type=str, default=os.environ.get('PS_HOSTS', ''),
+                        help='Comma-separated list of hostname:port pairs.')
+    parser.add_argument('--worker_hosts', type=str, default=os.environ.get('WORKER_HOSTS', ''),
+                        help='Comma-separated list of hostname:port pairs.')
+
+    # Parse args
+    opts = parser.parse_args()
+
+    if opts.worker_hosts:
+        opts.worker_hosts = opts.worker_hosts.split(',')
+    else:
+        opts.worker_hosts = []
+
+    if opts.ps_hosts:
+        opts.ps_hosts = opts.ps_hosts.split(',')
+    else:
+        opts.ps_hosts = []
+
+    return opts
+
+def make_tf_config(opts):
+    """Returns TF_CONFIG that can be used to set the environment variable necessary for distributed training"""
+    if all([opts.job_name is None, not opts.ps_hosts, not opts.worker_hosts]):
+        return {}
+    elif any([opts.job_name is None, not opts.ps_hosts, not opts.worker_hosts]):
+        tf.logging.warn('Distributed setting is incomplete. You must pass job_name, ps_hosts, and worker_hosts.')
+        if opts.job_name is None:
+            tf.logging.warn('Expected job_name of worker or ps. Received {}.'.format(opts.job_name))
+        if not opts.ps_hosts:
+            tf.logging.warn('Expected ps_hosts, list of hostname:port pairs. Got {}. '.format(opts.ps_hosts) +
+                            'Example: --ps_hosts "localhost:2224" or --ps_hosts "localhost:2224,localhost:2225')
+        if not opts.worker_hosts:
+            tf.logging.warn('Expected worker_hosts, list of hostname:port pairs. Got {}. '.format(opts.worker_hosts) +
+                            'Example: --worker_hosts "localhost:2222,localhost:2223"')
+        tf.logging.warn('Ignoring distributed arguments. Running single mode.')
+        return {}
+
+    tf_config = {
+        'task': {
+            'type': opts.job_name,
+            'index': opts.task_index
+        },
         'cluster': {
-            'chief': [worker_hosts[0]],
-            'worker': worker_hosts,
-            'ps': ps_hosts
+            'master': [opts.worker_hosts[0]],
+            'worker': opts.worker_hosts,
+            'ps': opts.ps_hosts
         },
         'environment': 'cloud'
     }
 
-    local_ip = 'localhost:' + TF_CONFIG['cluster'][task_type][task_index].split(':')[1]
-    TF_CONFIG['cluster'][task_type][task_index] = local_ip
-    if (task_type in ('chief', 'master')) or (task_type == 'worker' and task_index == 0):
-        TF_CONFIG['cluster']['worker'][task_index] = local_ip
-        TF_CONFIG['task']['type'] = 'chief'
+    # Nodes may need to refer to itself as localhost
+    local_ip = 'localhost:' + tf_config['cluster'][opts.job_name][opts.task_index].split(':')[1]
+    tf_config['cluster'][opts.job_name][opts.task_index] = local_ip
+    if opts.job_name == 'worker' and opts.task_index == 0:
+        tf_config['task']['type'] = 'master'
+        tf_config['cluster']['master'][0] = local_ip
+    return tf_config
 
-    os.environ['TF_CONFIG'] = json.dumps(TF_CONFIG)
-except KeyError as ex:
-    print(ex)
-    job_name = None
-    task_index = 0
-    ps_hosts = None
-    worker_hosts = None
+
+
+#
+# Snippet for distributed learning
+#
+#try:
+#    task_type = os.environ['JOB_NAME']
+#    task_index = int(os.environ['TASK_INDEX'])
+#    ps_hosts = os.environ['PS_HOSTS'].split(',')
+#    worker_hosts = os.environ['WORKER_HOSTS'].split(',')
+#    TF_CONFIG = {
+#        'task': {'type': task_type, 'index': task_index},
+#        'cluster': {
+#            'chief': [worker_hosts[0]],
+#            'worker': worker_hosts,
+#            'ps': ps_hosts
+#        },
+#        'environment': 'cloud'
+#    }
+#
+#    local_ip = 'localhost:' + TF_CONFIG['cluster'][task_type][task_index].split(':')[1]
+#    TF_CONFIG['cluster'][task_type][task_index] = local_ip
+#    if (task_type in ('chief', 'master')) or (task_type == 'worker' and task_index == 0):
+#        TF_CONFIG['cluster']['worker'][task_index] = local_ip
+#        TF_CONFIG['task']['type'] = 'chief'
+#
+#    os.environ['TF_CONFIG'] = json.dumps(TF_CONFIG)
+#except KeyError as ex:
+#    print(ex)
+#    job_name = None
+#    task_index = 0
+#    ps_hosts = None
+#    worker_hosts = None
 #
 #
 #
@@ -49,7 +128,7 @@ flags.DEFINE_integer("N_CLASSES",
                      "Number of prediction classes in the model")
 flags.DEFINE_string("train_data_dir",
                     get_data_path(
-                        dataset_name = 'artem-stable/artem-tiny-imagenet-1',
+                        dataset_name = 'artem-towa/artem-tiny-imagenet-example',
                         local_root = os.path.expanduser('~/Documents/Scratch/tiny_imagenet/'),
                         local_repo = 'tiny-imagenet-200',
                         path = 'train'
@@ -62,7 +141,7 @@ flags.DEFINE_string("train_data_dir",
                     "to use /logs/ when running on ClusterOne cloud.")
 flags.DEFINE_string("val_data_dir",
                     get_data_path(
-                        dataset_name = 'artem-stable/artem-tiny-imagenet-1',
+                        dataset_name = 'artem-towa/artem-tiny-imagenet-example',
                         local_root = os.path.expanduser('~/Documents/Scratch/tiny_imagenet/'),
                         local_repo = 'tiny-imagenet-200',
                         path = 'val/for_keras'
@@ -75,7 +154,7 @@ flags.DEFINE_string("val_data_dir",
                     "to use /logs/ when running on ClusterOne cloud.")
 flags.DEFINE_string("log_dir",
                     get_logs_path(
-                        os.path.expanduser('~/Documents/Scratch/tiny_imagenet/logs/')
+                        os.path.expanduser('~/Documents/Scratch/tiny_imagenet_2/logs/')
                     ),
                     "Path to dataset. It is recommended to use get_data_path()"
                     "to define your data directory.so that you can switch "
@@ -97,7 +176,7 @@ class TimeHistory(tf.train.SessionRunHook):
 def main(_):
     train_input = lambda: data_utils.input_fn(
         FLAGS.train_data_dir,
-        batch_size=64
+        batch_size=32
     )
 
     val_input = lambda: data_utils.input_fn(
@@ -117,7 +196,7 @@ def main(_):
         model_dir=FLAGS.log_dir,
         save_summary_steps=500,
         save_checkpoints_steps=500,
-        keep_checkpoint_max=3,
+        keep_checkpoint_max=1,
         log_step_count_steps=50
     )
 
@@ -129,17 +208,24 @@ def main(_):
 
     train_spec = tf.estimator.TrainSpec(
         input_fn=train_input,
-        max_steps=1e6,
-        hooks=[TimeHistory()]
+        max_steps=1e6
+      #  hooks=[TimeHistory()]
     )
     val_spec = tf.estimator.EvalSpec(
         input_fn=val_input,
         steps=None,
-        start_delay_secs=60,
+        start_delay_secs=600,
         throttle_secs=120
     )
 
     tf.estimator.train_and_evaluate(classifier, train_spec, val_spec)
 
 if __name__ == '__main__':
-    tf.app.run()
+    args = parse_args()
+
+    TF_CONFIG = make_tf_config(args)
+    tf.logging.debug('=' * 20 + ' TF_CONFIG ' + '=' * 20)
+    tf.logging.debug(TF_CONFIG)
+    os.environ['TF_CONFIG'] = json.dumps(TF_CONFIG)
+
+    main(args)
